4a91908f14a09be30f044ac18c6e42e2dd1e65f0
CJC 4K <CJC 4K@DESKTOP-E9CPK7V>
2018-07-10 15:58:29 -0500

    july102018

--- campbellscastleprep/App_Data/Models/all.generated.cs
+++ campbellscastleprep/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "f45ad651f0f8978d")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 
 // FILE: models.generated.cs
